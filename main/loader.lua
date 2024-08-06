a45388f1b63e03a8060d7a02adb02c8aa0758e87
Jake <jake52123@proton.me>
2024-08-06 09:26:31 +0900

    Update loader.lua

--- main/loader.lua
+++ main/loader.lua
@@ -1,13 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/jake1325/AimESP/main/modules/loader.lua"))()
-_G.AimbotEnabled = true
-_G.TeamCheck = true -- If set to true then the script would only lock your aim at enemy team members.
-_G.AimPart = "Head" -- Where the aimbot script would lock at.
-_G.Sensitivity = 0 -- How many seconds it takes for the aimbot script to officially lock onto the target's aimpart.
--- // FOV Settings //
-_G.CircleSides = 64 -- How many sides the FOV circle would have.
-_G.CircleColor = Color3.fromRGB(255, 255, 255) -- (RGB) Color that the FOV circle would appear as.
-_G.CircleTransparency = 0.7 -- Transparency of the circle.
-_G.CircleRadius = 100 -- The radius of the circle / FOV.
-_G.CircleFilled = false -- Determines whether or not the circle is filled.
-_G.CircleVisible = true -- Determines whether or not the circle is visible.
-_G.CircleThickness = 0 -- The thickness of the circle.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-Script/main/Aimbot%20Script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Blissful4992/ESPs/main/2D%20Box%20ESP/ESP.lua"))()
