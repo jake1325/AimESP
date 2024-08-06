4789a22433d784f123cc8dc920aaf4dfbbf5a5b6
Jake <jake52123@proton.me>
2024-08-06 09:26:49 +0900

    Delete modules/loader.lua

--- modules/loader.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-Script/main/Aimbot%20Script.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Blissful4992/ESPs/main/2D%20Box%20ESP/ESP.lua"))()
