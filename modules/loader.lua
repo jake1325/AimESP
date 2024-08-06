968f729ab4af02acea5f489b719411e8aaf2a80c
Jake <jake52123@proton.me>
2024-08-06 09:23:44 +0900

    Create loader.lua

---
+++ modules/loader.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-Script/main/Aimbot%20Script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Blissful4992/ESPs/main/2D%20Box%20ESP/ESP.lua"))()
